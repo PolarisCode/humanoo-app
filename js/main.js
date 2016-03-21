54a0729779fe132c9989465e096a38852857634c
Imran Aliyev <ialiyev@MacBook-Pro-Imran.local>
2016-03-22 00:51:24 +0400

    navigation between page has been developed

--- js/main.js
+++ js/main.js
@@ -1,15 +1,34 @@
 (function($){
   var menuItems = $('.menuItem'),
+      mainContent = $('#mainContent'),
+
+      loadPage = function(pageUrl){
+          mainContent.empty();
+          mainContent.load("content/" + pageUrl);
+      },
 
       menuClickHandler = function(){
         menuItems.on('click', function(){
+          activeMenu = $(this);
           menuItems.removeClass('active');
-          $(this).addClass('active');
+          activeMenu.addClass('active');
+
+          loadPage(activeMenu.attr('data-page'));
         })
+      },
+
+      loadInitialContent = function(){
+          var activePage = menuItems.filter('.active').attr('data-page');
+
+          if (activePage) {
+            loadPage(activePage);
+          }
       },
 
       init = function(){
         menuClickHandler();
+
+        loadInitialContent();
       }
 
       init();
